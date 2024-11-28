62d4f948a4bf0687fa9c101104233c0c2aba4c24
developerisnow <35399970+developerisnow@users.noreply.github.com>
2024-11-28 16:48:18 +0300

    v0.0.3: fixes before publish

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
-    }
-  }
+  coverageDirectory: 'coverage'
 }; 
